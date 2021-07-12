dd8dad9d526c34f93b507120bc3c4e24bcadf54b
minako-y <m.tp2uk@gmail.com>
2021-07-12 14:56:41 +0000

    7a修正途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,13 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.order(likes_count: :desc)
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_user).
+      sort {|a,b|
+        b.favorited_user.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorited_user.includes(:favorites).where(created_at: from...to).size
+      }
     @book = Book.new
   end
 
